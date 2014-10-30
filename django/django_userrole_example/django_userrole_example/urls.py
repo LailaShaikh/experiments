18ab1c18c8e3d56aed8047fbc8eee9c140fa2d08
Nava <nava.nmr@gmail.com>
2014-10-30 22:58:02 +0530

    css generation dynamically

--- django/django_userrole_example/django_userrole_example/urls.py
+++ django/django_userrole_example/django_userrole_example/urls.py
@@ -5,8 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_userrole_example.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+
     url(r'^home/$', 'myuser.views.home', name='home'),
+    url(r'^s/$', 'myuser.views.customize_page_style', name='customize'),
     url(r'^admin/', include(admin.site.urls)),
-                     
+
 )
--- django/django_userrole_example/myuser/views.py
+++ django/django_userrole_example/myuser/views.py
@@ -8,3 +8,42 @@ from .decorators import roles_required
 @roles_required('editor', 'admin')
 def home(request):
     return render(request, 'home.html')
+
+
+def generate_css_file(border, font_color, background, highlight,\
+                      title_color, des_color, font, filename='test.css',path=''):
+    #generate css file for the given params
+    wtext = ''
+    if font_color:
+        wtext = '''body {
+                    font-family: "Helvetica Neue",Helvetica,%s,sans-serif;
+                    font-size: 14px;
+                    line-height: 1.42857143;
+                    color: #%s;
+                    background-color: #%s;
+}
+'''
+        wtext =  wtext %(font, font_color, background)
+
+    #write
+    if wtext:
+        with open(filename, 'wb') as f:
+            f.write(wtext)
+
+def customize_page_style(request):
+    border = request.GET.get('border-color')
+    font_color = request.GET.get('font-color')
+    background = request.GET.get('background-color')
+    highlight = request.GET.get('highlight-color')
+    title_color = request.GET.get('title-color')
+    des_color = request.GET.get('des-color')
+    font = request.GET.get('font')
+
+    print border, font_color, background, highlight,title_color, des_color, font
+
+    val = request.GET
+    if len(val):
+        generate_css_file(border, font_color, background, highlight,\
+                          title_color, des_color, font)
+
+    return render(request, 'customize.html')
