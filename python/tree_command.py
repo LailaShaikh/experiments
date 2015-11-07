5f16bc89d771635637cfe2c943d5f19bca3585bb
Navaneethan R <navaneethan.r@edgenetworks.in>
2015-11-07 22:28:15 +0530

    partially done except print format

--- python/tree_command.py
+++ python/tree_command.py
@@ -13,15 +13,38 @@ import os
 '''
 
 
+def get_dirs_list(path):
+    return [os.path.join(path, d) for d in os.listdir(path) \
+            if os.path.isdir(os.path.join(path, d))]
+
+
+
+def get_files_only(path):
+    return [d for d in os.listdir(path) \
+            if os.path.isfile(os.path.join(path, d))]
+
+
 def print_line(files):
     for _f in files:
         print "\t |---- %s" %_f
 
 
+def start_from_dir(_path, level=1):
+    #print "\t o" * level
+
+    print "\n %s" % _path
+    _files = get_files_only(_path)
+    print_line(_files)
+
+
 def tree(top_path):
-    for root, dirs, files in os.walk(top_path):
-        print "%s" % root, dirs
-        print_line(files)
+    start_from_dir(top_path, level=1)
+    _dirs = get_dirs_list(top_path)
+    while _dirs:
+        curr_dir = _dirs.pop()
+        start_from_dir(curr_dir, level=2)
+        
+        _dirs.extend(get_dirs_list(curr_dir))
 
 
 if __name__ == '__main__':
