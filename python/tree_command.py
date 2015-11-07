d2385c18c438a3d34eba0df009014700f3da9466
Navaneethan R <navaneethan.r@edgenetworks.in>
2015-11-07 22:50:26 +0530

    output format added

--- python/tree_command.py
+++ python/tree_command.py
@@ -26,12 +26,12 @@ def get_files_only(path):
 
 def print_line(files, level=1):
     for _f in files:
-        print "\t" * level, "|---- %s" %_f
+        print "." * level * 4, "|",".... %s" %_f
 
 
 def start_from_dir(_path, level=1):
     #print "\t o" * level
-    print "\n", "\t" * level, _path
+    print "\n|", "." * level * 4, _path
     _files = get_files_only(_path)
     print_line(_files, level)
 
@@ -43,10 +43,40 @@ def tree(top_path):
         level, curr_dir = _dirs.pop()
         start_from_dir(curr_dir, level)
         
-        
         _dirs.extend([(level + 1, i) for i in get_dirs_list(curr_dir) if i])
 
 
 if __name__ == '__main__':
     #tree('/home/navaneethan/workspace/hirenew')
     tree('/home/navaneethan/workspace/autosourcing_engine/tests')
+
+
+# Output
+
+"""
+|  /home/navaneethan/workspace/autosourcing_engine/tests
+ | .... __init__.pyc
+ | .... __init__.py
+
+| .... /home/navaneethan/workspace/autosourcing_engine/tests/navaa
+
+| ........ /home/navaneethan/workspace/autosourcing_engine/tests/navaa/pp
+........ | .... __init__.pyc
+........ | .... __init__.py
+
+| .... /home/navaneethan/workspace/autosourcing_engine/tests/sourcer
+.... | .... test_timesjobs.py
+.... | .... __init__.pyc
+.... | .... test_shine.py
+.... | .... test_monster.pyc
+.... | .... test_shine.pyc
+.... | .... test_timesjobs.pyc
+.... | .... test_monster.py
+.... | .... __init__.py
+
+| ........ /home/navaneethan/workspace/autosourcing_engine/tests/sourcer/__pycache__
+........ | .... test_shine.cpython-27-PYTEST.pyc
+........ | .... test_timesjobs.cpython-27-PYTEST.pyc
+........ | .... test_monster.cpython-27-PYTEST.pyc
+
+"""
