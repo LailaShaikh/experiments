b3e46b35f25c7e880c1d171aefa2ffb0e7322404
Navaneethan R <navaneethan.r@edgenetworks.in>
2016-01-27 16:07:11 +0530

    map exercise function added

--- go/my_exercise.go
+++ go/my_exercise.go
@@ -1,10 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 func add(b int, a int) int {
 	return a + b
 }
+
+func MapExercise(){
+	mymap := make(map[string]int)
+	mymap["nava"] = 9
+	fmt.Println("My Initial Map is", mymap)
+	power, exists := mymap["test"]
+	fmt.Println(power, exists, len(mymap))
+	delete(mymap, "deletekeyhere")
+	for k,v := range mymap {
+		fmt.Println(k,v)
+	}
+}
+
+func arrTest(){
+	ta := make([]int, 0, 20)
+	ta = append(ta, 4)
+	ta = append(ta, 5)
+	for v := 0; v < 10; v++{
+		ta = append(ta, v)
+		fmt.Println(ta)
+	}
+	tan := ta[3:6]
+	tan[0] = 11
+	fmt.Println(tan, ta)
+	for i:=0; i<20; i++{
+		ta = append(ta, int(rand.Int31n(1000)))
+	}
+	sort.Ints(ta)
+
+	dest := make([]int, 20)
+	copy(dest, ta)
+	fmt.Println("Dest:=",dest, ta)
+
+	MapExercise()
+}
+
+
+/*
+func extrPow(sai []*SG) []int{
+	pow := make([]int, len(sai))
+	for index, saaa := range sai{
+		pow[index] = saaa.Power
+	}
+	return pow
+}
+*/
 
 func arrayExample() {
 	arr := make([]int, 0, 10)
@@ -15,7 +65,7 @@ func arrayExample() {
 	for i := 0; i<40; i++ {
 		arr = append(arr, i)
 		if intC != cap(arr){
-			fmt.Println("Curr Cap", cap(arr))
+			//fmt.Println("Curr Cap", cap(arr))
 		}
 	}
 	fmt.Println("Final Cap is", cap(arr))
@@ -27,4 +77,5 @@ func main(){
 	s = append(s, 22);
 	fmt.Println("Intial Array", s)
 	arrayExample()
+	arrTest()
 }
